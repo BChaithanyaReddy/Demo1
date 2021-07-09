c220297f0ed7541d798bd921089e56bbcf5534f3
BChaithanyaReddy <bandichaithanyareddy@gmail.com>
2021-07-09 11:15:12 +0530

    First Sample

---
+++ Sample.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+   const browser = await puppeteer.launch({headless:false});
+  const page = await browser.newPage();
+  await page.goto("https://www.amazon.in/");
+title = await page.evaluate(() => {
+return document.querySelector("#nav-xshop-container").textContent.trim();});
+console.log(title);
+ browser.close();
+})();
